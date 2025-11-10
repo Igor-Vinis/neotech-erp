335f09398f17d58c7cb3514fc667b0ee6b7680a5
Igor Vinicius <igorvinicius010905@gmail.com>
2025-11-10 11:47:58 -0300

    finalizando os métodos do ClienteFileDAO.

--- src/infrastructure/persistence/ClienteFileDAO.java
+++ src/infrastructure/persistence/ClienteFileDAO.java
@@ -8,16 +8,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 public class ClienteFileDAO implements ClienteRepository{
 
     private final FilerManager filerManager = new FilerManager();
     private final String path = "data/Clientes/clientes.csv";
-
-    @Override
-    public Optional<Cliente> buscarPorEmail(String email) {
-        return Optional.empty();
-    }
 
     @Override
     public void salvar(Cliente obj) {
@@ -43,17 +39,45 @@ public class ClienteFileDAO implements ClienteRepository{
 
     @Override
     public void deletar(UUID id) {
+        List<String> linhas = filerManager.lerTodasLinhas(path);
+        var novaLista = linhas.stream()
+                .filter(l -> {var c = Cliente.fromCSV(l);
+                    boolean manter = !(Objects.equals(id, c.getId()));
+                    return manter;})
+                .toList();
 
+        filerManager.escreverLinhas(path,novaLista);
     }
 
     @Override
     public List<Cliente> buscarTodos() {
-        return List.of();
+        List<String> linhas = filerManager.lerTodasLinhas(path);
+        List<Cliente> clientes = linhas.stream().map(Cliente::fromCSV).toList();
+        return clientes;
     }
 
     @Override
     public Optional<Cliente> buscarPorId(UUID id) {
-        return Optional.empty();
+
+        List<String> linhas = filerManager.lerTodasLinhas(path);
+        for(String linha : linhas){
+            var cliente = Cliente.fromCSV(linha);
+            if (Objects.equals(id, cliente.getId())){
+                return Optional.of(cliente);
+            }
+        } return Optional.empty();
+    }
+
+    @Override
+    public Optional<Cliente> buscarPorEmail(String email) {
+        List<String> linhas = filerManager.lerTodasLinhas(path);
+        for(String linha : linhas){
+            var cliente = Cliente.fromCSV(linha);
+            if (Objects.equals(email, cliente.getEmail())){
+                return Optional.of(cliente);
+            }
+        } return Optional.empty();
+
     }
 
 
